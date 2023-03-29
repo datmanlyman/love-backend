56fab925e5136925c4a1438ead6b3f7efad86f32
datmanlyman <jeewern2@gmail.com>
2023-03-29 21:36:47 +0800

    able to make dataframes now

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pandas as pd
 
 
 def getDatabase():
@@ -14,25 +15,50 @@ def getCollection(dbName):
     return dbName["love_checker"]
 
 
+'''
 def getDocuments(collectionName):
     document1 = {
-        "message": "I love you",
+        "message": "<3",
         "love": 0,
         "neutral": 0,
         "hate": 0
     }
 
     document2 = {
-        "message": "Please leave me alone, I do not like you",
+        "message": "</3",
         "love": 0,
         "neutral": 0,
         "hate": 0
     }
 
     collectionName.insert_many([document1, document2])
+'''
 
 
 if __name__ == "__main__":
     dbName = getDatabase()
     collectionName = getCollection(dbName)
-    getDocuments(collectionName)
+    love_df = pd.DataFrame(columns=["message", "sentiment"])
+
+    for record in collectionName.find():
+        winner = ""
+
+        if record["love"] == record["hate"] == record["neutral"]:
+            winner = "neutral"
+        elif record["love"] > record["hate"] and record["love"] > record["neutral"]:  # love is most
+            winner = "love"
+        elif record["hate"] > record["love"] and record["hate"] > record["neutral"]:  # hate is most
+            winner = "hate"
+        elif record["neutral"] > record["love"] and record["neutral"] > record["hate"]:  # neutral is most
+            winner = "neutral"
+        elif record["love"] == record["neutral"]:
+            winner = "love"
+        elif record["hate"] == record["neutral"]:
+            winner = "hate"
+        elif record["love"] == record["hate"]:
+            winner = "neutral"
+
+        love_df = love_df.append(
+            {"message": record["message"], "sentiment": winner}, ignore_index=True)
+
+    print(love_df)
